40ae8801e54c7a5393f435d2e267e1a398e441de
Michael Hart <michael.hart.au@gmail.com>
2018-12-30 13:03:27 -0500

    Update version to 0.10.0 – revert this and `npm version` appropriately

--- publish.sh
+++ publish.sh
@@ -23,17 +23,18 @@ echo $VERSION > /tmp/lambci.txt
 
 aws s3api put-object --bucket lambci --key fn/lambci-build-${VERSION}.zip --body build/lambda.zip --acl public-read
 
-aws s3api copy-object --copy-source lambci/fn/lambci-build-${VERSION}.zip --bucket lambci --key fn/lambci-build-latest.zip --acl public-read &
+# aws s3api copy-object --copy-source lambci/fn/lambci-build-${VERSION}.zip --bucket lambci --key fn/lambci-build-latest.zip --acl public-read &
 
-aws s3api put-object --bucket lambci --key fn/latest.txt --body /tmp/lambci.txt --acl public-read &
+# aws s3api put-object --bucket lambci --key fn/latest.txt --body /tmp/lambci.txt --acl public-read &
 
 npm run template
-aws s3api put-object --bucket lambci --key templates/lambci.template --body build/versioned.yaml --acl public-read &
+# aws s3api put-object --bucket lambci --key templates/lambci.template --body build/versioned.yaml --acl public-read &
 aws s3api put-object --bucket lambci --key templates/template.yaml --body build/versioned.yaml --acl public-read &
 
 for region in $REGIONS; do
-  aws s3api copy-object --region $region --copy-source lambci/fn/lambci-build-${VERSION}.zip --bucket lambci-${region} --key fn/lambci-build-${VERSION}.zip --acl public-read && \
-  aws s3api copy-object --region $region --copy-source lambci-${region}/fn/lambci-build-${VERSION}.zip --bucket lambci-${region} --key fn/lambci-build-latest.zip --acl public-read &
+  aws s3api copy-object --region $region --copy-source lambci/fn/lambci-build-${VERSION}.zip --bucket lambci-${region} --key fn/lambci-build-${VERSION}.zip --acl public-read &
+  # aws s3api copy-object --region $region --copy-source lambci/fn/lambci-build-${VERSION}.zip --bucket lambci-${region} --key fn/lambci-build-${VERSION}.zip --acl public-read && \
+  # aws s3api copy-object --region $region --copy-source lambci-${region}/fn/lambci-build-${VERSION}.zip --bucket lambci-${region} --key fn/lambci-build-latest.zip --acl public-read &
 done
 
 for job in $(jobs -p); do

67e5e7e3065cec3df27dc2566f8405668d577c70
Sebastian Tom Gellweiler <sebastian.gellweiler@dm.de>
2019-03-24 21:56:58 +0100

    Subscribe to github status event to properly end ecs builds

--- actions/build.js
+++ actions/build.js
@@ -1,8 +1,8 @@
 var path = require('path')
-var EventEmitter = require('events')
 var spawn = require('child_process').spawn
 var async = require('neo-async')
 var AWS = require('aws-sdk')
+var BuildInfo = require('./buildInfo.js')
 var utils = require('../utils')
 var log = require('../utils/log')
 var config = require('../utils/config')
@@ -313,63 +313,3 @@ function prepareDockerConfig(buildConfig) {
   }
   return utils.merge(defaultDockerConfig, buildConfig)
 }
-
-function BuildInfo(buildData, context) {
-  this.startedAt = new Date()
-  this.endedAt = null
-
-  this.status = 'pending'
-  this.statusEmitter = new EventEmitter()
-
-  // Any async functions to run on 'finish' should be added to this array,
-  // and be of the form: function(build, cb)
-  this.statusEmitter.finishTasks = []
-
-  this.project = buildData.project
-  this.buildNum = buildData.buildNum || 0
-
-  this.repo = buildData.repo || this.project.replace(/^gh\//, '')
-
-  if (buildData.trigger) {
-    var triggerPieces = buildData.trigger.split('/')
-    this.trigger = buildData.trigger
-    this.eventType = triggerPieces[0] == 'pr' ? 'pull_request' : 'push'
-    this.prNum = triggerPieces[0] == 'pr' ? +triggerPieces[1] : 0
-    this.branch = triggerPieces[0] == 'push' ? triggerPieces[1] : (buildData.branch || 'master')
-  } else {
-    this.eventType = buildData.eventType
-    this.prNum = buildData.prNum
-    this.branch = buildData.branch
-    this.trigger = this.prNum ? `pr/${this.prNum}` : `push/${this.branch}`
-  }
-
-  this.event = buildData.event
-  this.isPrivate = buildData.isPrivate
-  this.isRebuild = buildData.isRebuild
-
-  this.branch = buildData.branch
-  this.cloneRepo = buildData.cloneRepo || this.repo
-  this.checkoutBranch = buildData.checkoutBranch || this.branch
-  this.commit = buildData.commit
-  this.baseCommit = buildData.baseCommit
-  this.comment = buildData.comment
-  this.user = buildData.user
-
-  this.isFork = this.cloneRepo != this.repo
-
-  this.committers = buildData.committers
-
-  this.config = null
-  this.cloneDir = path.join(config.BASE_BUILD_DIR, this.repo)
-
-  this.requestId = context.awsRequestId
-  this.logGroupName = context.logGroupName
-  this.logStreamName = context.logStreamName
-
-  this.token = ''
-  this.logUrl = ''
-  this.lambdaLogUrl = ''
-  this.buildDirUrl = ''
-  this.error = null
-}
-
--- sources/webhook.js
+++ sources/webhook.js
@@ -47,6 +47,23 @@ exports.build = function(event, context, cb) {
       return done(null, {msg: 'pong'})
     }
 
+    if (eventType == 'status') {
+      var innerEvent
+
+      try {
+        innerEvent = JSON.parse(event.body)
+      } catch (e) {
+        throw new Error(`Could not parse webhook body as JSON: ${e.message}`)
+      }
+
+      lambda.invoke({
+        InvocationType: 'Event',
+        FunctionName: process.env.AWS_LAMBDA_FUNCTION_NAME,
+        Payload: JSON.stringify(Object.assign({action: 'updateStatus'}, innerEvent)),
+      }, done)
+      return
+    }
+
     var buildData
 
     try {
